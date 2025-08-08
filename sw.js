0ee7d1d2dc77e9dd84bcdc0991b5995f676e13d4
Jenstijn <tom@dohle.nl>
2025-08-08 11:35:21 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+
+const CACHE_NAME = 'lingualab-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './sw.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  // Network-first for HTML, cache-first for others
+  if (req.headers.get('accept')?.includes('text/html')) {
+    event.respondWith(
+      fetch(req).then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((c) => c.put(req, resClone));
+        return res;
+      }).catch(() => caches.match(req))
+    );
+  } else {
+    event.respondWith(
+      caches.match(req).then((cached) => cached || fetch(req).then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((c) => c.put(req, resClone));
+        return res;
+      }))
+    );
+  }
+});
